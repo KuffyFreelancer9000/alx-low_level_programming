32a0070ee95f186d866903003f18d7698048d2d0
KuffyFreelancer9000 <KuffyFreelancer9000@gmail.com>
2022-10-04 23:43:50 -0700

    Advanced Gold

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,13 +9,13 @@
  **/
 int numchecker(char *s)
 {
-  int i;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      if (s[i] < '0' || s[i] > '9')
-	return (0);
-    }
-  return (1);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+}
+return (1);
 }
 /**
  * main - find least amount of coins needed for change
