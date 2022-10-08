ce1bcebeac59bdf921f7e0bf0174d4d4569c29a1
KuffyFreelancer9000 <KuffyFreelancer9000@gmail.com>
2022-10-07 19:33:41 -0700

    Advanced Coding

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,33 @@
-# an array of chars and initializes it with
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+/**
+ * find_len - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_len(char *str)
+{
+int len = 0;
+
+while (*str++)
+len++;
+
+return (len);
+}
+
+/**
+ * create_xarray - Creates an array of chars and initializes it with
+ *                 the character 'x'. Adds a terminating null byte.
+
 *                 the character 'x'. Adds a terminating null byte.
 * @size: The size of the array to be initialized.
 *
